15b4501e51518d59225e3745dbdefb7ac3ef9186
sabbirofc <mdsabbirhowlader420@gmail.com>
2023-08-27 21:28:08 +0600

    first commit

---
+++ js/phone.js
@@ -0,0 +1,15 @@
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+    // console.log(phones);
+    displayPhones(phones)
+}
+
+
+const displayPhones = phones => {
+    phones.forEach(phone =>{
+        console.log(phone)
+    })
+}
+loadPhone();
